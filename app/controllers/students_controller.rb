03ce0c50ee9d6ffdd0e5fe7045d5f095d15bdc57
jinook929 <jinook929@gmail.com>
2021-03-10 18:36:47 -0500

    commit tree b9918d57876284245c7410cfe4a3abdf6aaa75f0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -6,6 +7,22 @@ class StudentsController < ApplicationController
   end
 
   def show
+  end
+
+  def activate
+    student = Student.find(params[:id])
+    # if student.active
+    #   student.active = false
+    # else
+    #   student.active = true
+    # end
+
+    # student.active = !student.active 
+    
+    student.toggle(:active)
+    student.save
+    redirect_to student_path(student)
+    # redirect_to "/students/#{student.id}"
   end
 
   private
